4f17c80d6cedceff5fc57f9699fe00eab685bb19
rakibulislam161363 <rakibulislam161363@gmail.com>
2025-01-27 21:14:35 +0600

    nav end

--- react-app/src/component/Nav.jsx
+++ react-app/src/component/Nav.jsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
+
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
@@ -21,22 +21,15 @@ const pages = ['About Us', 'Pricing', 'Customers', 'Solutions'];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  
 
   const handleOpenNavMenu = event => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = event => {
-    setAnchorElUser(event.currentTarget);
-  };
+ 
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+ 
 
   return (
     <AppBar position="static" sx={{ backgroundColor: 'black' }}>
